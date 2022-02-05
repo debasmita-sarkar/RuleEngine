230a535da927a27fb6efba4306af43ebcf0423e6
Debasmita Sarkar <debasmita.sarkar@mavenir.com>
2022-02-06 03:03:03 +0530

    Added payment type for video and refactored payment service class.

--- src/main/java/com/demo/rule/service/PaymentService.java
+++ src/main/java/com/demo/rule/service/PaymentService.java
@@ -6,6 +6,7 @@ import com.demo.rule.actions.impl.PackingSlipGenerator;
 import com.demo.rule.model.PaymentRequest;
 import com.demo.rule.payments.PAYMENT_TYPES;
 import com.demo.rule.utility.Constant;
+import com.demo.rule.utility.MEMBERSHIP_ACTION_TYPE;
 import com.demo.rule.utility.PAYSLIP_TYPE;
 import com.demo.rule.utility.RECEIVER_TYPE;
 import lombok.extern.slf4j.Slf4j;
@@ -29,8 +30,20 @@ public class PaymentService {
             result = executePhysicalProduct(request, result, PAYSLIP_TYPE.SHIPPING);
         }else if(request.getPaymentType().compareTo(PAYMENT_TYPES.BOOK.name()) ==0){
             result = executePhysicalProduct(request, result, PAYSLIP_TYPE.ROYALTY);
+        }else if(request.getPaymentType().compareTo(PAYMENT_TYPES.MEMBERSHIP_ACTIVATE.name()) ==0){
+            updateMemberState(request, MEMBERSHIP_ACTION_TYPE.ACTIVATE);
+        }else if(request.getPaymentType().compareTo(PAYMENT_TYPES.MEMBERSHIP_UPGRADE.name()) ==0){
+            updateMemberState(request, MEMBERSHIP_ACTION_TYPE.UPGRADE);
+        }else if(request.getPaymentType().compareTo(PAYMENT_TYPES.VIDEO.name()) ==0){
+            IActionExecutor executor = factory.getInstance(PAYSLIP_TYPE.VIDEO.name());
+            return executor.execute(request,PAYSLIP_TYPE.VIDEO.name());
         }
         return result;
+    }
+
+    private void updateMemberState(PaymentRequest request, MEMBERSHIP_ACTION_TYPE upgrade) {
+        IActionExecutor executor = factory.getInstance(MEMBERSHIP_ACTION_TYPE.UPGRADE.toString());
+        executor.execute(request, upgrade.toString());
     }
 
     private boolean executePhysicalProduct(PaymentRequest request, boolean result, PAYSLIP_TYPE shipping) {
